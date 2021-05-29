4944b30ea733c13b4abc3cb02c341b30ea8f4177
xmiao-meng <3306541996@qq.com>
2021-05-29 17:39:25 +0800

    first init

---
+++ m_core/src/main/java/top/xm_m/m_shops/m_core/MCoreApplication.java
@@ -0,0 +1,11 @@
+package top.xm_m.m_shops.m_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MCoreApplication.class);
+    }
+}
---
+++ m_db/src/main/java/top/xm_m/m_shops/m_db/MDbApplication.java
@@ -0,0 +1,11 @@
+package top.xm_m.m_shops.m_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MDbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MDbApplication.class);
+    }
+}
